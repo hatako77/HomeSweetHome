25a98f1c0a732fb4aca7d8d90bd8a09ef892acd7
hatako77 <37298920+hatako77@users.noreply.github.com>
2026-07-04 10:18:58 +0330

    Update config.h

--- include/config.h
+++ include/config.h
@@ -6,23 +6,23 @@
 // ===== WiFi Configuration =====
 #define WIFI_SSID "Ali TT"
 #define WIFI_PASSWORD "2150068486"
-#define WIFI_TIMEOUT 30 // seconds
+#define WIFI_TIMEOUT 30
 
 // ===== OTA Configuration =====
 #define OTA_HOST "hatako77.github.io"
 #define OTA_PATH "/firmware/"
 #define OTA_VERSION_FILE "version.txt"
 #define OTA_FIRMWARE_FILE "firmware.bin"
-#define OTA_CHECK_INTERVAL 3600000 // 1 hour
+#define OTA_CHECK_INTERVAL 3600000
 
 // ===== Web Server Configuration =====
 #define WEB_SERVER_PORT 80
 #define MAX_OTA_CLIENTS 1
 
 // ===== Update Configuration =====
-#define OTA_TIMEOUT 60000 // 60 seconds
-#define REDIRECT_TIMEOUT 5 // seconds
-#define MAX_SKETCH_SIZE 0x200000 // 2MB
+#define OTA_TIMEOUT 60000
+#define REDIRECT_TIMEOUT 5
+#define MAX_SKETCH_SIZE 0x200000
 
 // ===== Debug Configuration =====
 #define DEBUG_ENABLED true
@@ -31,11 +31,10 @@
 #if DEBUG_ENABLED
     #define DEBUG_PRINT(x) Serial.print(x)
     #define DEBUG_PRINTLN(x) Serial.println(x)
-    #define DEBUG_PRINTF(x, y) Serial.printf(x, y)
+    // حذف DEBUG_PRINTF که مشکل داشت
 #else
     #define DEBUG_PRINT(x)
     #define DEBUG_PRINTLN(x)
-    #define DEBUG_PRINTF(x, y)
 #endif
 
 #endif
