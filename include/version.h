8969a4695a8e64c2678ce843ee5894cc8c2cadc2
hatako77 <37298920+hatako77@users.noreply.github.com>
2026-07-04 09:35:07 +0330

    Create version.h

---
+++ include/version.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#define FW_NAME        "HomeSweetHome"
+
+#define FW_MAJOR       1
+#define FW_MINOR       0
+#define FW_PATCH       0
+
+#define FW_VERSION     "1.0.0"
+
+#define GITHUB_OWNER   "YOUR_GITHUB_USERNAME"
+#define GITHUB_REPO    "HomeSweetHome"
