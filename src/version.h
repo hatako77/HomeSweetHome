b8cf6f194bc47cdceca9dfe5660ed4ef7081a52c
hatako77 <37298920+hatako77@users.noreply.github.com>
2026-07-04 12:00:55 +0330

    Create version.h

---
+++ src/version.h
@@ -0,0 +1,25 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+#define VERSION_MAJOR 1
+#define VERSION_MINOR 0
+#define VERSION_PATCH 0
+#define BUILD_NUMBER 1
+
+#define STRINGIFY(x) #x
+#define TOSTRING(x) STRINGIFY(x)
+
+#define VERSION_STRING TOSTRING(VERSION_MAJOR) "." \
+                       TOSTRING(VERSION_MINOR) "." \
+                       TOSTRING(VERSION_PATCH)
+
+#define FULL_VERSION_STRING TOSTRING(VERSION_MAJOR) "." \
+                            TOSTRING(VERSION_MINOR) "." \
+                            TOSTRING(VERSION_PATCH) "." \
+                            TOSTRING(BUILD_NUMBER)
+
+extern const char* FIRMWARE_VERSION;
+extern const char* BUILD_DATE;
+extern const char* BUILD_TIME;
+
+#endif
