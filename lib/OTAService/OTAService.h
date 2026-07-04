b925c69b51509f143806e7b3348ad125415035d4
hatako77 <37298920+hatako77@users.noreply.github.com>
2026-07-04 22:51:23 +0330

    Update OTAService.h

--- lib/OTAService/OTAService.h
+++ lib/OTAService/OTAService.h
@@ -6,27 +6,29 @@
 #include <Update.h>
 #include <ArduinoJson.h>
 
+struct OTAStatus
+{
+    bool running = false;
+    bool finished = false;
+    bool success = false;
+
+    uint32_t downloaded = 0;
+    uint32_t total = 0;
+
+    uint8_t percent = 0;
+
+    float speedKB = 0;
+
+    uint32_t eta = 0;
+
+    String state = "Idle";
+    String error = "";
+};
 class OTAService {
 public:
     bool checkForUpdate();
     bool updateFirmware();
 
-    struct OTAStatus {
-        bool running;
-        bool finished;
-        bool success;
-    
-        uint32_t downloaded;
-        uint32_t total;
-        uint8_t percent;
-    
-        float speedKB;
-    
-        uint32_t eta;
-    
-        String state;
-        String error;
-    };
     
 void setVersionURL(const String& url);
     void setCurrentVersion(const String& version);
