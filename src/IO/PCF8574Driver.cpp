3068404e4be2e536d87b9f26c608ec53e56e2371
hatako77 <37298920+hatako77@users.noreply.github.com>
2026-07-05 01:17:42 +0330

    Create PCF8574Driver.cpp

---
+++ src/IO/PCF8574Driver.cpp
@@ -0,0 +1,44 @@
+#include "IO/PCF8574Driver.h"
+
+void PCF8574Driver::begin()
+{
+    Wire.begin();
+
+    for (uint8_t i = 0; i < DEVICE_COUNT; i++)
+    {
+        uint8_t addr = 0x20 + i;
+
+        pcf[i] = new PCF8574(addr);
+
+        connected[i] = pcf[i]->begin();
+
+        Serial.print("PCF ");
+        Serial.print(addr, HEX);
+
+        if (connected[i])
+            Serial.println(" OK");
+        else
+            Serial.println(" NOT FOUND");
+    }
+}
+
+bool PCF8574Driver::isConnected(uint8_t index)
+{
+    return connected[index];
+}
+
+bool PCF8574Driver::read(uint8_t index, uint8_t pin)
+{
+    if (!connected[index])
+        return false;
+
+    return pcf[index]->read(pin);
+}
+
+void PCF8574Driver::write(uint8_t index, uint8_t pin, bool state)
+{
+    if (!connected[index])
+        return;
+
+    pcf[index]->write(pin, state);
+}
