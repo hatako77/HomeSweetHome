b473673ac087098073fa209b3b1224487259656f
hatako77 <37298920+hatako77@users.noreply.github.com>
2026-07-04 17:43:51 +0330

    Create config.h

---
+++ include/config.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#define WIFI_SSID      "Ali TT"
+#define WIFI_PASSWORD  "2150068486"
+
+#define GITHUB_USER    "hatako77"
+#define GITHUB_REPO    "HomeSweetHome"
+
+#define VERSION_FILE   "version.json"
+#define FIRMWARE_FILE  "firmware.bin"
