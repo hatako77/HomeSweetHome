09d5ffda9c56e443765a2267f96b77bdbf7cc134
hatako77 <37298920+hatako77@users.noreply.github.com>
2026-07-04 18:11:20 +0330

    Update version.h

--- include/version.h
+++ include/version.h
@@ -1,4 +1,11 @@
 #pragma once
 
-#define FIRMWARE_NAME      "Home Sweet Home"
-#define FIRMWARE_VERSION   "1.0.0"
+#define FW_NAME        "Home Sweet Home"
+
+#define FW_VERSION     "1.0.0"
+
+#define FW_AUTHOR      "Ali"
+
+#define FW_BUILD_DATE  __DATE__
+
+#define FW_BUILD_TIME  __TIME__
