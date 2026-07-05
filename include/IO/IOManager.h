cd48a57ee8cd0b160b379b9509ffa1ae79b6b840
hatako77 <37298920+hatako77@users.noreply.github.com>
2026-07-05 09:55:20 +0330

    Update IOManager.h

--- include/IO/IOManager.h
+++ include/IO/IOManager.h
@@ -1,29 +1,38 @@
 #ifndef IO_MANAGER_H
 #define IO_MANAGER_H
 
-#include "IO/PCF8574Driver.h"
+#include <Arduino.h>
+
 #include "IO/IOConfig.h"
+#include "IO/PCF8574Driver.h"
 
 class IOManager
 {
 public:
 
-    static const uint8_t TOTAL_IO = 64;
+    static const uint8_t MAX_IO = 64;
 
     void begin();
 
-    IOConfig& get(uint8_t index);
+    void update();
 
-    bool getState(uint8_t index);
+    bool setState(uint8_t id,bool state);
 
-    void setState(uint8_t index, bool state);
+    bool getState(uint8_t id);
 
-    void toggle(uint8_t index);
+    IOChannel* get(uint8_t id);
+
+    uint8_t count();
 
 private:
-    PCF8574Driver driver;
-    IOConfig io[TOTAL_IO];
 
+    PCF8574Driver driver;
+
+    IOChannel channels[MAX_IO];
+
+    uint8_t ioCount = 0;
 };
+
+extern IOManager ioManager;
 
 #endif
