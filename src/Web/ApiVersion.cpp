9469ebf64ef0c025ced555ce1f6e839bff5c518e
hatako77 <37298920+hatako77@users.noreply.github.com>
2026-07-05 11:00:01 +0330

    Create ApiVersion.cpp

---
+++ src/Web/ApiVersion.cpp
@@ -0,0 +1,29 @@
+#include "Web/ApiVersion.h"
+
+#include <ArduinoJson.h>
+
+#include "Web/WebServerService.h"
+#include "Core/Version.h"
+
+namespace ApiVersion
+{
+
+void registerRoutes(WebServerService& web)
+{
+    auto& server = web.server();
+
+    server.on("/api/version", HTTP_GET, [&server]()
+    {
+        JsonDocument doc;
+
+        doc["app"] = APP_NAME;
+        doc["version"] = APP_VERSION;
+
+        String out;
+        serializeJson(doc, out);
+
+        server.send(200, "application/json", out);
+    });
+}
+
+}
