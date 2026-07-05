117cd7e56371ab6f3894cd4ea88fce3ca0fa2623
hatako77 <37298920+hatako77@users.noreply.github.com>
2026-07-05 10:58:48 +0330

    Create Pages.cpp

---
+++ src/Web/Pages.cpp
@@ -0,0 +1,28 @@
+#include "Web/Pages.h"
+#include "Web/WebServerService.h"
+
+#include "WebUI/index_html.h"
+#include "WebUI/ota_html.h"
+
+namespace Pages
+{
+
+void registerRoutes(WebServerService& web)
+{
+    auto& server = web.server();
+
+    server.on("/", HTTP_GET, []()
+    {
+        server.send_P(200, "text/html", INDEX_HTML);
+    });
+
+    server.on("/ota", HTTP_GET, []()
+    {
+        server.send_P(200, "text/html", OTA_HTML);
+    });
+
+    // بعداً
+    // server.on("/settings", HTTP_GET, ...);
+}
+
+}
