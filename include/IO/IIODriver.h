3ef328adc0e66288444dfc383693cfa350e5da5f
hatako77 <37298920+hatako77@users.noreply.github.com>
2026-07-06 22:37:24 +0330

    Update IIODriver.h

--- include/IO/IIODriver.h
+++ include/IO/IIODriver.h
@@ -7,18 +7,13 @@ class IIODriver
 {
 public:
     virtual ~IIODriver() {}
-
     virtual bool begin() = 0;
-
     virtual void update() = 0;
-
     virtual bool read(uint8_t device, uint8_t pin) = 0;
-
     virtual bool write(uint8_t device, uint8_t pin, bool state) = 0;
-
     virtual bool isConnected(uint8_t device) = 0;
-
     virtual uint8_t deviceCount() = 0;
+    virtual uint8_t pinCount(uint8_t device) = 0;
 };
 
 #endif
