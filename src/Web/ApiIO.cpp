650f5ec2546423b1090777c548142bd23bfc2a7c
hatako77 <37298920+hatako77@users.noreply.github.com>
2026-07-06 16:00:47 +0330

    Update ApiIO.cpp

--- src/Web/ApiIO.cpp
+++ src/Web/ApiIO.cpp
@@ -5,74 +5,131 @@
 #include "Web/WebServerService.h"
 #include "IO/IOManager.h"
 
+namespace
+{
+
+const char* iconToString(IOIcon icon)
+{
+    switch(icon)
+    {
+        case IOIcon::Light:  return "light";
+        case IOIcon::Fan:    return "fan";
+        case IOIcon::Socket: return "socket";
+        case IOIcon::TV:     return "tv";
+        case IOIcon::Pump:   return "pump";
+        default:             return "switch";
+    }
+}
+
+const char* typeToString(IOType type)
+{
+    switch(type)
+    {
+        case IOType::DigitalInput:
+            return "input";
+
+        case IOType::DigitalOutput:
+            return "output";
+
+        default:
+            return "unknown";
+    }
+}
+
+}
+
 namespace ApiIO
 {
 
 void registerRoutes(WebServerService& web)
 {
     auto& server = web.server();
+
     server.on("^\\/api\\/io\\/([0-9]+)$", HTTP_POST, [&server]()
     {
-        String uri = server.uri();    
-        int pos = uri.lastIndexOf('/');    
-        uint8_t id = uri.substring(pos + 1).toInt();    
-        IOChannel* ch = ioManager.get(id);    
-        if (ch == nullptr)
+        String uri = server.uri();
+
+        int pos = uri.lastIndexOf('/');
+
+        uint8_t id = uri.substring(pos + 1).toInt();
+
+        IOChannel* ch = ioManager.get(id);
+
+        if(ch == nullptr)
         {
-            server.send(404, "text/plain", "Not Found");
+            server.send(404,"text/plain","Not Found");
             return;
-        }    
-        bool state = !ioManager.getState(id);    
-        ioManager.setState(id, state);    
-        server.send(200, "text/plain", state ? "ON" : "OFF");
+        }
+
+        bool state = !ioManager.getState(id);
+
+        ioManager.setState(id,state);
+
+        server.send(200,"text/plain",state ? "ON" : "OFF");
     });
-    
-    
+
     server.on("/api/io/toggle", HTTP_POST, [&server]()
     {
-        if (!server.hasArg("id"))
+        if(!server.hasArg("id"))
         {
-            server.send(400, "text/plain", "Missing id");
+            server.send(400,"text/plain","Missing id");
             return;
-        }    
-        uint8_t id = server.arg("id").toInt();    
-        if (id >= ioManager.count())
+        }
+
+        uint8_t id = server.arg("id").toInt();
+
+        if(id >= ioManager.count())
         {
-            server.send(404, "text/plain", "Invalid ID");
+            server.send(404,"text/plain","Invalid ID");
             return;
-        }    
+        }
+
         ioManager.toggle(id);
-        server.send(200, "application/json", "{\"success\":true}");
+
+        server.send(200,"application/json","{\"success\":true}");
     });
 
-
-
-
-    
     server.on("/api/io", HTTP_GET, [&server]()
     {
         JsonDocument doc;
+
         JsonArray arr = doc.to<JsonArray>();
 
-        for (uint8_t i = 0; i < ioManager.count(); i++)
+        for(uint8_t i = 0; i < ioManager.count(); i++)
         {
             IOChannel* ch = ioManager.get(i);
+
+            if(ch == nullptr)
+                continue;
+
             JsonObject obj = arr.add<JsonObject>();
-            obj["id"] = i;
+
+            obj["id"] = ch->id;
             obj["name"] = ch->name;
-            obj["state"] = ch->state;
+
+            obj["state"] = ioManager.getState(i);
+
             obj["enabled"] = ch->enabled;
             obj["activeLow"] = ch->activeLow;
-            obj["icon"] = (uint8_t)ch->icon;
+
+            obj["favorite"] = ch->favorite;
+
+            obj["type"] = typeToString(ch->type);
+            obj["icon"] = iconToString(ch->icon);
+
+            obj["roomId"] = ch->roomId;
+            obj["groupId"] = ch->groupId;
+
             obj["board"] = ch->board;
             obj["address"] = ch->address;
             obj["pin"] = ch->pin;
-            obj["type"] = (uint8_t)ch->type;
         }
 
         String out;
-        serializeJson(doc, out);
-        server.send(200, "application/json", out);
+
+        serializeJson(doc,out);
+
+        server.send(200,"application/json",out);
     });
 }
 
