57a27fd18855b49b20c47bc009dcf2dbf03336bf
hatako77 <37298920+hatako77@users.noreply.github.com>
2026-07-06 16:45:19 +0330

    Update Pages.cpp

--- src/Web/Pages.cpp
+++ src/Web/Pages.cpp
@@ -1,8 +1,9 @@
 #include "Web/Pages.h"
 #include "Web/WebServerService.h"
-
 #include "WebUI/index_html.h"
 #include "WebUI/ota_html.h"
+#include "WebUI/style_css.h"
+#include "WebUI/app_js.h"
 
 namespace Pages
 {
@@ -10,18 +11,24 @@ namespace Pages
 void registerRoutes(WebServerService& web)
 {
     auto& server = web.server();
-
-    server.on("/", HTTP_GET, [&]()
+    server.on("/", HTTP_GET, [&server]()
     {
         server.send_P(200, "text/html", INDEX_HTML);
     });
-
-    server.on("/ota", HTTP_GET, [&]()
+    server.on("/ota", HTTP_GET, [&server]()
     {
         server.send_P(200, "text/html", OTA_HTML);
     });
+    server.on("/style.css", HTTP_GET, [&server]()
+    {
+        server.send_P(200, "text/css", STYLE_CSS);
+    });
+    server.on("/app.js", HTTP_GET, [&server]()
+    {
+        server.send_P(200, "application/javascript", APP_JS);
+    });
 
-    // بعداً
+    // بعداً:
     // server.on("/settings", HTTP_GET, ...);
 }
 
