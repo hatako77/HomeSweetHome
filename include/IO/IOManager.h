99ce9c9b04214da74991258855442901430fb2c9
hatako77 <37298920+hatako77@users.noreply.github.com>
2026-07-06 23:27:45 +0330

    Update IOManager.h

--- include/IO/IOManager.h
+++ include/IO/IOManager.h
@@ -20,7 +20,7 @@ public:
     bool toggle(uint8_t id);
     void begin();
     void update();
-    IOChannel* getChannel(uint8_t id);
+    IOChannel* getChannel(uint8_t id) const;
     uint8_t count() const;
     IIODriver* getDriver(uint8_t driverId);
 private:
