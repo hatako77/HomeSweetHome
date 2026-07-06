a93382cc30d81ea7c2e3a0d7c40c3cb741742931
hatako77 <37298920+hatako77@users.noreply.github.com>
2026-07-07 00:16:08 +0330

    Create RoomManager.h

---
+++ include/Room/RoomManager.h
@@ -0,0 +1,35 @@
+#ifndef ROOM_MANAGER_H
+#define ROOM_MANAGER_H
+
+#include <Arduino.h>
+#include "Room/Room.h"
+
+class RoomManager
+{
+public:
+
+    static constexpr uint8_t MAX_ROOMS = 32;
+
+    void begin();
+
+    uint8_t count() const;
+
+    Room* get(uint8_t id);
+
+    bool add(const Room& room);
+
+    bool remove(uint8_t id);
+
+
+private:
+
+    Room rooms[MAX_ROOMS];
+
+    uint8_t roomCount = 0;
+};
+
+
+extern RoomManager roomManager;
+
+
+#endif
