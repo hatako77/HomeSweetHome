4aa697942d3b3547a9a81ae1bd59324eda026212
hatako77 <37298920+hatako77@users.noreply.github.com>
2026-07-07 00:16:31 +0330

    Create RoomManager.cpp

---
+++ src/Room/RoomManager.cpp
@@ -0,0 +1,59 @@
+#include "Room/RoomManager.h"
+
+
+RoomManager roomManager;
+
+
+void RoomManager::begin()
+{
+    roomCount = 0;
+}
+
+
+uint8_t RoomManager::count() const
+{
+    return roomCount;
+}
+
+
+Room* RoomManager::get(uint8_t id)
+{
+    if (id >= roomCount)
+        return nullptr;
+
+    return &rooms[id];
+}
+
+
+bool RoomManager::add(const Room& room)
+{
+    if (roomCount >= MAX_ROOMS)
+        return false;
+
+
+    rooms[roomCount] = room;
+    rooms[roomCount].id = roomCount;
+
+    roomCount++;
+
+    return true;
+}
+
+
+bool RoomManager::remove(uint8_t id)
+{
+    if (id >= roomCount)
+        return false;
+
+
+    for (uint8_t i = id; i < roomCount - 1; i++)
+    {
+        rooms[i] = rooms[i + 1];
+        rooms[i].id = i;
+    }
+
+
+    roomCount--;
+
+    return true;
+}
