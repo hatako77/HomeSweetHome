446e217c022915b879c3b792fdb005f5bba71a3f
hatako77 <37298920+hatako77@users.noreply.github.com>
2026-07-06 20:41:53 +0330

    Update PCF8574Driver.h

--- include/IO/PCF8574Driver.h
+++ include/IO/PCF8574Driver.h
@@ -1,23 +1,23 @@
 #ifndef PCF8574_DRIVER_H
 #define PCF8574_DRIVER_H
-
+#include "IIODriver.h"
 #include <Arduino.h>
 #include <Wire.h>
 #include <PCF8574.h>
 
-class PCF8574Driver
+class PCF8574Driver : public IIODriver
 {
 public:
     static const uint8_t DEVICE_COUNT = 8;
 
-    void begin();
-    void update();
+bool begin() override;
+void update() override;
 
-    bool isConnected(uint8_t index);
-    bool read(uint8_t index, uint8_t pin);
-    void write(uint8_t index, uint8_t pin, bool state);
+bool isConnected(uint8_t device) override;
+bool read(uint8_t device, uint8_t pin) override;
+bool write(uint8_t device, uint8_t pin, bool state) override;
 
-    uint8_t getDeviceCount();
+uint8_t deviceCount() override;
 
 private:
     PCF8574* pcf[DEVICE_COUNT];
