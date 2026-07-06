3b79304f98668732db3329346ab0f7abdb078007
hatako77 <37298920+hatako77@users.noreply.github.com>
2026-07-06 20:03:17 +0330

    Update IOManager.h

--- include/IO/IOManager.h
+++ include/IO/IOManager.h
@@ -9,37 +9,21 @@
 class IOManager
 {
 public:
-
     static const uint8_t MAX_IO = 64;
-    bool write(uint8_t id, bool state);
-    
-    bool read(uint8_t id);
-    
-    bool on(uint8_t id);
-    
-    bool off(uint8_t id);
-    
+    bool write(uint8_t id, bool state);    
+    bool read(uint8_t id);    
+    bool on(uint8_t id);    
+    bool off(uint8_t id);    
     bool toggle(uint8_t id);
     void begin();
-
     void update();
-
-bool setState(uint8_t id, bool state);
-bool getState(uint8_t id);
     IOChannel* get(uint8_t id);
-
     PCF8574Driver& getDriver();
     uint8_t count();
-
 private:
-
     PCF8574Driver driver;
-
     IOChannel channels[MAX_IO];
-
     uint8_t ioCount = 0;
 };
-
 extern IOManager ioManager;
-
 #endif
