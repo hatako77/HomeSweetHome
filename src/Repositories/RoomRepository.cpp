3f5c378ee5cc0dc94053044fc2596e85a5aac85d
hatako77 <37298920+hatako77@users.noreply.github.com>
2026-07-07 16:51:14 +0330

    Update RoomRepository.cpp

--- src/Repositories/RoomRepository.cpp
+++ src/Repositories/RoomRepository.cpp
@@ -23,3 +23,32 @@ uint8_t RoomRepository::count() const
 {
     return roomCount;
 }
+
+
+bool RoomRepository::load()
+{
+        if (!FileStorage::exists(Paths::Rooms))
+        return false;
+    File f = FileStorage::open(Paths::Rooms, "r");
+    if (!f)
+        return false;
+    JsonDocument doc;
+    if (deserializeJson(doc, f))
+    {
+        f.close();
+        return false;
+    }
+    JsonArray arr = doc.as<JsonArray>();
+    for (JsonObject o : arr)
+    {
+        Room room;
+        room.id = o["id"] | 0;
+        room.name = o["name"] | "Room";
+        room.icon = o["icon"] | "home";
+        room.enabled = o["enabled"] | true;
+        room.favorite = o["favorite"] | false;
+        repository.add(room);
+    }
+    f.close();
+    return true;
+}
