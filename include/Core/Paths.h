8eeff9fe72fc5dd1b1e4952e0056516883f32b5b
hatako77 <37298920+hatako77@users.noreply.github.com>
2026-07-07 16:32:29 +0330

    Create Paths.h

---
+++ include/Core/Paths.h
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace Paths
+{
+    constexpr char Rooms[]  = "/rooms.json";
+    constexpr char IO[]     = "/io.json";
+    constexpr char Groups[] = "/groups.json";
+    constexpr char Scenes[] = "/scenes.json";
+    constexpr char Settings[] = "/settings.json";
+}
