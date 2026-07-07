8d59b95703f35d94bdfe14c3fb89c4acc9a8b5c0
hatako77 <37298920+hatako77@users.noreply.github.com>
2026-07-07 17:18:52 +0330

    Update IORepository.h

--- include/Repositories/IORepository.h
+++ include/Repositories/IORepository.h
@@ -1,35 +1,23 @@
 #pragma once
 
 #include <Arduino.h>
-#include "IO/IOChannel.h"
+#include "IO/IOConfig.h"
 
 class IORepository
 {
 public:
     static constexpr uint16_t MAX_CHANNELS = 64;
-
     void begin();
-
     uint16_t count() const;
-
     IOChannel* get(uint16_t id);
-
     bool add(const IOChannel& channel);
-
     bool update(const IOChannel& channel);
-
     bool remove(uint16_t id);
-
     bool load();
-
     bool save();
-
 private:
     IOChannel channels[MAX_CHANNELS];
-
     uint16_t channelCount = 0;
-
     uint16_t nextId = 1;
 };
-
 extern IORepository ioRepository;
