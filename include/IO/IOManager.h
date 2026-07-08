4361c92f1160b5a38c3b0bc1498f98ceb0a0d06a
hatako77 <37298920+hatako77@users.noreply.github.com>
2026-07-08 15:16:49 +0330

    Update IOManager.h

--- include/IO/IOManager.h
+++ include/IO/IOManager.h
@@ -11,26 +11,26 @@
 class IOManager
 {
 public:
-    uint8_t countByRoom(uint8_t roomId) const;
-    static constexpr uint8_t MAX_IO = 64;
-    bool write(uint8_t id, bool state);    
-    bool read(uint8_t id) const;    
-    bool on(uint8_t id);    
-    bool off(uint8_t id);    
-    bool toggle(uint8_t id);
+    uint16_t countByRoom(uint16_t roomId) const;
+    static constexpr uint16_t MAX_IO = 64;
+    bool write(uint16_t id, bool state);    
+    bool read(uint16_t id) const;    
+    bool on(uint16_t id);    
+    bool off(uint16_t id);    
+    bool toggle(uint16_t id);
     void begin();
     void update();
-    IOChannel* getChannel(uint8_t id);
-    const IOChannel* getChannel(uint8_t id) const;
-    uint8_t count() const;
-    IIODriver* getDriver(uint8_t driverId);
+    IOChannel* getChannel(uint16_t id);
+    const IOChannel* getChannel(uint16_t id) const;
+    uint16_t count() const;
+    IIODriver* getDriver(uint16_t driverId);
     bool add(const IOChannel& channel);
     bool update(const IOChannel& channel);
     bool remove(uint16_t id);
 private:
-    static constexpr uint8_t MAX_DRIVERS = 8;
+    static constexpr uint16_t MAX_DRIVERS = 8;
     IIODriver* drivers[MAX_DRIVERS];
-    uint8_t driverCount = 0;
+    uint16_t driverCount = 0;
     PCF8574Driver pcfDriver;
     IOChannel channels[MAX_IO];
     uint16_t channelCount = 0;
