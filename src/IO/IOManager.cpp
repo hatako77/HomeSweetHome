74fc086e0f6050feb29ce2fee18fe652c288fdcc
hatako77 <37298920+hatako77@users.noreply.github.com>
2026-07-08 16:17:36 +0330

    Update IOManager.cpp

--- src/IO/IOManager.cpp
+++ src/IO/IOManager.cpp
@@ -15,42 +15,34 @@ void IOManager::begin()
 
     for (uint16_t d = 0; d < driverCount; d++)
     {
-    IIODriver* drv = drivers[d];
-
-    for (uint16_t device = 0; device < drv->deviceCount(); device++)
-    {
+        IIODriver* drv = drivers[d];
+        for (uint16_t device = 0; device < drv->deviceCount(); device++)
+        {
         if (!drv->isConnected(device))
             continue;
-
-        for (uint16_t pin = 0; pin < drv->pinCount(device); pin++)
-        {
-            IOChannel ch;
-
-            ch.id = channelCount;
-            ch.name = "IO " + String(channelCount  + 1);
-            // TODO: Load icon from storage.
-            ch.icon = IOIcon::Light;
-            // TODO: Load channel configuration from storage.
-            ch.type = IOType::DigitalOutput;
-            ch.state = false;
-            ch.enabled = true;
-            ch.activeLow = false;
-
-            ch.address.driverId = d;
-            ch.address.device = device;
-            ch.address.pin = pin;
-
-            ch.roomId = 0;
-            ch.groupId = 0;
-            ch.favorite = false;
-
-            add(ch);
+            for (uint16_t pin = 0; pin < drv->pinCount(device); pin++)
+            {
+                IOChannel ch;    
+                ch.id = channelCount;
+                ch.name = "IO " + String(channelCount  + 1);
+                ch.icon = IOIcon::Light;
+                ch.type = IOType::DigitalOutput;
+                ch.state = false;
+                ch.enabled = true;
+                ch.activeLow = false;    
+                ch.address.driverId = d;
+                ch.address.device = device;
+                ch.address.pin = pin;    
+                ch.roomId = 0;
+                ch.groupId = 0;
+                ch.favorite = false;    
+                add(ch);
+            }
         }
-    }
-}
-
+    }    
     Serial.print("channelCount: ");
     Serial.println(channelCount);
+    load();
 }
 
 void IOManager::update()
@@ -209,3 +201,87 @@ bool IOManager::update(const IOChannel& channel)
     return false;
 }
 
+bool IOManager::save() const
+{
+    File f = LittleFS.open("/io.json", "w");
+
+    if (!f)
+        return false;
+
+    JsonDocument doc;
+
+    JsonArray arr = doc.to<JsonArray>();
+
+    for (uint8_t i = 0; i < count(); i++)
+    {
+        auto* ch = getChannel(i);
+        JsonObject o = arr.add<JsonObject>();
+        o["id"] = ch->id;        
+        o["name"] = ch->name;        
+        o["enabled"] = ch->enabled;
+        o["favorite"] = ch->favorite;        
+        o["activeLow"] = ch->activeLow;        
+        o["type"] = static_cast<uint8_t>(ch->type);
+        o["icon"] = static_cast<uint8_t>(ch->icon);        
+        o["roomId"] = ch->roomId;
+        o["groupId"] = ch->groupId;        
+        o["driverId"] = ch->address.driverId;
+        o["device"] = ch->address.device;
+        o["pin"] = ch->address.pin;
+    }
+
+    serializeJson(doc, f);
+
+    f.close();
+
+    return true;
+}
+
+bool IOManager::load()
+{
+    if (!LittleFS.exists("/io.json"))
+        return false;
+
+    File f = LittleFS.open("/io.json", "r");
+
+    if (!f)
+        return false;
+
+    JsonDocument doc;
+
+    deserializeJson(doc, f);
+
+    JsonArray arr = doc.as<JsonArray>();
+
+    uint8_t i = 0;
+
+    for (JsonObject o : arr)
+    {
+        if (i >= count())
+            break;
+
+        auto* ch = getChannel(i);
+
+        ch->name = o["name"] | ch->name;
+        ch->enabled = o["enabled"] | true;
+        ch->activeLow = o["activeLow"] | false;
+        IOType type;
+        if (typeFromValue(o["type"] | 1, type))
+            ch->type = type;
+        IOIcon icon;        
+        if (iconFromValue(o["icon"] | 0, icon))
+            ch->icon = icon;
+        ch->favorite = o["favorite"] | false;
+        ch->roomId = o["roomId"] | 0;
+        ch->groupId = o["groupId"] | 0;
+        ch->favorite = o["favorite"] | false;
+        ch->address.driverId = o["driverId"] | 0;
+        ch->address.device = o["device"] | 0;
+        ch->address.pin = o["pin"] | 0;
+        i++;
+    }
+
+    f.close();
+
+    return true;
+}
