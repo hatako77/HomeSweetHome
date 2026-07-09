b3c71ea31a464e502237b6347e75afcd13096b62
hatako77 <37298920+hatako77@users.noreply.github.com>
2026-07-09 22:04:38 +0330

    Update ApiSettings.cpp

--- src/Web/ApiSettings.cpp
+++ src/Web/ApiSettings.cpp
@@ -10,49 +10,55 @@
 namespace ApiSettings
 {
 
-void registerRoutes(WebServerService& web)
-{
-    auto& server = web.server();
-
-    server.on("/api/settings/io", HTTP_POST, [&server]()
+    void registerRoutes(WebServerService& web)
     {
-        if (!server.hasArg("plain"))
-        {
-            server.send(400, "text/plain", "Missing body");
-            return;
-        }
-
-        JsonDocument doc;
-
-        if (deserializeJson(doc, server.arg("plain")))
-        {
-            server.send(400, "text/plain", "Invalid JSON");
-            return;
-        }
-
-        JsonArray arr = doc.as<JsonArray>();
-
-        uint8_t i = 0;
-
-        for (JsonObject o : arr)
-        {
-            if (i >= ioManager.count())
-                break;
-
-            IOChannel* ch = ioManager.getChannel(i);
-
-            ch->name = o["name"] | ch->name;
-            ch->enabled = o["enabled"] | true;
-            ch->activeLow = o["activeLow"] | false;
-            IOType type;
-            if (typeFromValue(o["type"] | 1, type))
-                ch->type = type;
-            i++;
-        }
-
-            ioManager.save();
-        server.send(200, "application/json", "{\"success\":true}");
-    });
-}
-
+        auto& server = web.server();    
+        server.on("/api/settings/io",  HTTP_POST,    
+            [](AsyncWebServerRequest *request)
+            {
+            },    
+            nullptr,    
+            [](AsyncWebServerRequest *request,
+               uint8_t *data,
+               size_t len,
+               size_t,
+               size_t)
+            {
+                JsonDocument doc;    
+                if (deserializeJson(doc, data, len))
+                {
+                    request->send(
+                        400,
+                        "text/plain",
+                        "Invalid JSON"
+                    );
+                    return;
+                }    
+                JsonArray arr = doc.as<JsonArray>();    
+                uint16_t i = 0;    
+                for (JsonObject o : arr)
+                {
+                    if (i >= ioManager.count())
+                        break;    
+                    IOChannel* ch = ioManager.getChannel(i);    
+                    if (!ch)
+                    {
+                        i++;
+                        continue;
+                    }    
+                    ch->name = o["name"] | ch->name;
+                    ch->enabled = o["enabled"] | ch->enabled;
+                    ch->activeLow = o["activeLow"] | ch->activeLow;    
+                    IOType type;
+                    if (typeFromValue(o["type"] | (uint8_t)ch->type, type))
+                        ch->type = type;    
+                    IOIcon icon;
+                    if (iconFromValue(o["icon"] | (uint8_t)ch->icon, icon))
+                        ch->icon = icon;    
+                    i++;
+                }    
+                ioManager.save();    
+                request->send(200, "application/json", "{\"success\":true}");
+            });
+    }
 }
