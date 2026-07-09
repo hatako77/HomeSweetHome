e087ff55e7ddfe43a653d09ea2fccdd30f051a91
hatako77 <37298920+hatako77@users.noreply.github.com>
2026-07-09 22:06:54 +0330

    Update ApiOTA.cpp

--- src/Web/ApiOTA.cpp
+++ src/Web/ApiOTA.cpp
@@ -10,60 +10,74 @@ extern OTAService ota;
 namespace ApiOTA
 {
 
-void registerRoutes(WebServerService& web)
-{
-    auto& server = web.server();
-
-    server.on("/api/ota/version", HTTP_GET, [&server]()
+    void registerRoutes(WebServerService& web)
     {
-        JsonDocument doc;
-
-        doc["current"] = ota.getCurrentVersion();
-        doc["remote"] = ota.getRemoteVersion();
-        doc["update"] = ota.checkForUpdate();
-
-        String out;
-        serializeJson(doc, out);
-
-        server.send(200, "application/json", out);
-    });
-
-    server.on("/api/ota/status", HTTP_GET, [&server]()
-    {
-        JsonDocument doc;
-
-        auto st = ota.getStatus();
-
-        doc["running"] = st.running;
-        doc["finished"] = st.finished;
-        doc["success"] = st.success;
-        doc["downloaded"] = st.downloaded;
-        doc["total"] = st.total;
-        doc["percent"] = st.percent;
-        doc["speed"] = st.speedKB;
-        doc["eta"] = st.eta;
-        doc["state"] = st.state;
-        doc["error"] = st.error;
-
-        String out;
-        serializeJson(doc, out);
-
-        server.send(200, "application/json", out);
-    });
-
-        server.on("/api/ota/update", HTTP_POST, [&server]()
-        {
-            server.send(200, "text/plain", "OTA STARTED");
-        
-            xTaskCreate([](void*)
+        auto& server = web.server();    
+        server.on("/api/ota/version", HTTP_GET,
+            [](AsyncWebServerRequest *request)
             {
-                bool ok = ota.updateFirmware();
-        
-                Serial.println(ok ? "OTA OK" : "OTA FAILED");
-        
-                vTaskDelete(NULL);
-            }, "ota_task", 8192, NULL, 1, NULL);
-        });
-
+                JsonDocument doc;    
+                doc["current"] = ota.getCurrentVersion();
+                doc["remote"] = ota.getRemoteVersion();
+                doc["update"] = ota.checkForUpdate();    
+                String out;
+                serializeJson(doc, out);    
+                request->send(
+                    200,
+                    "application/json",
+                    out
+                );
+            });
+    
+        server.on("/api/ota/status",
+            HTTP_GET,
+            [](AsyncWebServerRequest *request)
+            {
+                JsonDocument doc;    
+                auto st = ota.getStatus();    
+                doc["running"]    = st.running;
+                doc["finished"]   = st.finished;
+                doc["success"]    = st.success;
+                doc["downloaded"] = st.downloaded;
+                doc["total"]      = st.total;
+                doc["percent"]    = st.percent;
+                doc["speed"]      = st.speedKB;
+                doc["eta"]        = st.eta;
+                doc["state"]      = st.state;
+                doc["error"]      = st.error;    
+                String out;
+                serializeJson(doc, out);    
+                request->send(
+                    200,
+                    "application/json",
+                    out
+                );
+            });
+    
+        server.on("/api/ota/update",
+            HTTP_POST,
+            [](AsyncWebServerRequest *request)
+            {
+                request->send(
+                    200,
+                    "text/plain",
+                    "OTA STARTED"
+                );    
+                xTaskCreate(
+                    [](void*)
+                    {
+                        bool ok = ota.updateFirmware();    
+                        Serial.println(
+                            ok ? "OTA OK" : "OTA FAILED"
+                        );    
+                        vTaskDelete(nullptr);
+                    },
+                    "ota_task",
+                    8192,
+                    nullptr,
+                    1,
+                    nullptr
+                );
+            });
     }
 }
