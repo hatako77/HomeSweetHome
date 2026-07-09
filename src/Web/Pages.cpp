8bfa19b059fdfb6d3fd82613a82c46bf48e82fe5
hatako77 <37298920+hatako77@users.noreply.github.com>
2026-07-09 19:33:37 +0330

    Update Pages.cpp

--- src/Web/Pages.cpp
+++ src/Web/Pages.cpp
@@ -1,32 +1,28 @@
 #include "Web/Pages.h"
 #include "Web/WebServerService.h"
-#include <LittleFS.h>
-#include "WebUI/ota_html.h"
 
+#include <LittleFS.h>
 
 namespace Pages
 {
 
-    void registerRoutes(WebServerService& web)
+void registerRoutes(WebServerService& web)
+{
+    auto& server = web.server();
+
+    if (!LittleFS.begin(true))
     {
-        auto& server = web.server();
-    
-        if (!LittleFS.begin(true))
-        {
-            Serial.println("LittleFS Mount Failed");
-        }
-    
-        server.serveStatic("/", LittleFS, "/").setDefaultFile("index.html");
-    
-        server.serveStatic("/style.css", LittleFS, "/style.css");
-        server.serveStatic("/app.js", LittleFS, "/app.js");
-    
-        server.serveStatic("/icons/", LittleFS, "/icons/");
-    
-        server.on("/ota", HTTP_GET, [&server]()
-        {
-            server.send_P(200, "text/html", OTA_HTML);
-        });
+        Serial.println("LittleFS Mount Failed");
+        return;
     }
+
+    server.serveStatic("/", LittleFS, "/").setDefaultFile("index.html");
+
+    server.serveStatic("/style.css", LittleFS, "/style.css");
+
+    server.serveStatic("/app.js", LittleFS, "/app.js");
+
+    server.serveStatic("/icons/", LittleFS, "/icons/");
+}
 
 }
