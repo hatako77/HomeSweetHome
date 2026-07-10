8cd453b0b882aa03ba6f40d031f55ec381a57dec
hatako77 <37298920+hatako77@users.noreply.github.com>
2026-07-10 22:38:52 +0330

    Update Notifier.h

--- include/Web/Notifier.h
+++ include/Web/Notifier.h
@@ -9,16 +9,13 @@
 class Notifier
 {
 public:
-
     static void reload();
-
     static void channelChanged(const IOChannel& channel);
-
-    static void otaStatus(
-        const OTAStatus& status,
-        const String& current,
-        const String& remote
-    );
+    static void sceneAdded(const Scene& scene);
+    static void sceneUpdated(const Scene& scene);    
+    static void sceneRemoved(uint16_t id);    
+    static void sceneExecuted(uint16_t id);
+    static void otaStatus(const OTAStatus& status, const String& current, const String& remote);
 };
 
 #endif
