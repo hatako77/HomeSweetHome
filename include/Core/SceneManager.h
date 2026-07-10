5f1708d3bbf0899e2c3c51e01dc9c18bff5e671d
hatako77 <37298920+hatako77@users.noreply.github.com>
2026-07-10 21:49:46 +0330

    Create SceneManager.h

---
+++ include/Core/SceneManager.h
@@ -0,0 +1,41 @@
+#ifndef SCENE_MANAGER_H
+#define SCENE_MANAGER_H
+
+#include "Core/Scene.h"
+#include "Core/IOManager.h"
+
+class SceneManager
+{
+public:
+
+    static constexpr uint8_t MAX_SCENES = 32;
+
+    void begin();
+
+    bool load();
+    bool save();
+
+    bool add(const Scene& scene);
+
+    bool update(const Scene& scene);
+
+    bool remove(uint16_t id);
+
+    Scene* get(uint16_t id);
+
+    const Scene* get(uint16_t id) const;
+
+    uint16_t count() const;
+
+    bool execute(uint16_t id);
+
+private:
+
+    Scene scenes[MAX_SCENES];
+
+    uint16_t sceneCount = 0;
+};
+
+extern SceneManager sceneManager;
+
+#endif
