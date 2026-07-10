b24d645c9a89f83bff38ad91e2e54307a3a880ff
hatako77 <37298920+hatako77@users.noreply.github.com>
2026-07-10 13:31:49 +0330

    Update OTAService.h

--- include/OTA/OTAService.h
+++ include/OTA/OTAService.h
@@ -1,29 +1,11 @@
 #ifndef OTA_SERVICE_H
 #define OTA_SERVICE_H
-
+#include "OTA/OTAStatus.h"
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include <Update.h>
 #include <ArduinoJson.h>
 
-struct OTAStatus
-{
-    bool running = false;
-    bool finished = false;
-    bool success = false;
-
-    uint32_t downloaded = 0;
-    uint32_t total = 0;
-
-    uint8_t percent = 0;
-
-    float speedKB = 0;
-
-    uint32_t eta = 0;
-
-    String state = "Idle";
-    String error = "";
-};
 class OTAService {
 public:
     bool checkForUpdate();
