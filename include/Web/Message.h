47b93366b2bd4b29c475d40fd342055d1cb14b85
hatako77 <37298920+hatako77@users.noreply.github.com>
2026-07-10 20:15:57 +0330

    Create Message.h

---
+++ include/Web/Message.h
@@ -0,0 +1,27 @@
+#ifndef MESSAGE_H
+#define MESSAGE_H
+
+#include <ArduinoJson.h>
+
+class Message
+{
+public:
+
+    JsonDocument json;
+
+    Message(const String& type,
+            const String& action)
+    {
+        json["type"] = type;
+        json["action"] = action;
+    }
+
+    String serialize()
+    {
+        String s;
+        serializeJson(json, s);
+        return s;
+    }
+};
+
+#endif
