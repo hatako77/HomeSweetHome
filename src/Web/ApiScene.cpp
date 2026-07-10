b8f2e9b24a233aaaa1cd2d1400bbd109ce27e380
hatako77 <37298920+hatako77@users.noreply.github.com>
2026-07-10 22:13:47 +0330

    Create ApiScene.cpp

---
+++ src/Web/ApiScene.cpp
@@ -0,0 +1,8 @@
+#include "Web/ApiScene.h"
+#include <ArduinoJson.h>
+#include "Core/SceneManager.h"
+#include "Web/Message.h"
+#include "Web/WebSocketService.h"
+
+extern SceneManager sceneManager;
+extern WebSocketService websocket;
