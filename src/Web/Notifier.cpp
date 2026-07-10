604b0572e74cb07fca481e941b6792177162e42b
hatako77 <37298920+hatako77@users.noreply.github.com>
2026-07-10 22:40:13 +0330

    Update Notifier.cpp

--- src/Web/Notifier.cpp
+++ src/Web/Notifier.cpp
@@ -1,10 +1,55 @@
 #include "Web/Notifier.h"
+#include "Core/Scene.h"
+
 
 extern WebSocketService websocket;
 
 void Notifier::reload()
 {
     Message msg("reload");
+
+    websocket.send(msg);
+}
+void Notifier::sceneAdded(const Scene& scene)
+{
+    Message msg("scene","added");
+
+    msg.json["id"] = scene.id;
+    msg.json["name"] = scene.name;
+    msg.json["icon"] = scene.icon;
+    msg.json["favorite"] = scene.favorite;
+    msg.json["enabled"] = scene.enabled;
+
+    websocket.send(msg);
+}
+
+void Notifier::sceneUpdated(const Scene& scene)
+{
+    Message msg("scene","updated");
+
+    msg.json["id"] = scene.id;
+    msg.json["name"] = scene.name;
+    msg.json["icon"] = scene.icon;
+    msg.json["favorite"] = scene.favorite;
+    msg.json["enabled"] = scene.enabled;
+
+    websocket.send(msg);
+}
+
+void Notifier::sceneRemoved(uint16_t id)
+{
+    Message msg("scene","removed");
+
+    msg.json["id"] = id;
+
+    websocket.send(msg);
+}
+
+void Notifier::sceneExecuted(uint16_t id)
+{
+    Message msg("scene","executed");
+
+    msg.json["id"] = id;
 
     websocket.send(msg);
 }
