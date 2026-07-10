f11da5dadbbbb39805b61c6f7cee5e2138449637
hatako77 <37298920+hatako77@users.noreply.github.com>
2026-07-10 22:10:34 +0330

    Update SceneManager.h

--- include/Core/SceneManager.h
+++ include/Core/SceneManager.h
@@ -9,31 +9,22 @@ class SceneManager
 public:
 
     static constexpr uint8_t MAX_SCENES = 32;
-
     void begin();
-
     bool load();
     bool save();
-
     bool add(const Scene& scene);
-
     bool update(const Scene& scene);
-
     bool remove(uint16_t id);
-
     Scene* get(uint16_t id);
-
     const Scene* get(uint16_t id) const;
-
     uint16_t count() const;
-
     bool execute(uint16_t id);
 
 private:
 
     Scene scenes[MAX_SCENES];
-
     uint16_t sceneCount = 0;
+    uint16_t nextId = 1;
 };
 
 extern SceneManager sceneManager;
