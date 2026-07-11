aad756f2ecbd638393c75b13b3ea2dd4e3791225
hatako77 <37298920+hatako77@users.noreply.github.com>
2026-07-11 16:16:37 +0330

    Update SceneManager.h

--- include/Core/SceneManager.h
+++ include/Core/SceneManager.h
@@ -4,6 +4,14 @@
 #include "Core/Scene.h"
 #include "IO/IOManager.h"
 
+struct SceneTimer
+{
+    bool active = false;
+    uint16_t channelId = 0;
+    bool targetState = false;
+    uint32_t expiresAt = 0;
+};
+
 class SceneManager
 {
 public:
@@ -22,8 +30,12 @@ public:
     bool execute(uint16_t id);
     Scene* getAt(uint16_t index);    
     const Scene* getAt(uint16_t index) const;
-private:
+    void update();    
+    void addTimer(uint16_t channelId,bool targetState,uint32_t durationMs);
 
+private:
+    static constexpr uint8_t MAX_TIMERS = 32;    
+    SceneTimer timers[MAX_TIMERS];
     Scene scenes[MAX_SCENES];
     uint16_t sceneCount = 0;
     uint16_t nextId = 1;
