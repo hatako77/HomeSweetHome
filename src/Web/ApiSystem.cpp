2a35442ce2d8136b7c86bbf9c90cd2116a82581a
hatako77 <37298920+hatako77@users.noreply.github.com>
2026-07-13 21:34:23 +0330

    Create ApiSystem.cpp

---
+++ src/Web/ApiSystem.cpp
@@ -0,0 +1,41 @@
+#include "Web/ApiSystem.h"
+
+#include "Core/RoomManager.h"
+#include "Core/SceneManager.h"
+#include "IO/IOManager.h"
+
+#include <WiFi.h>
+
+#include "version.h"
+
+void ApiSystem::registerRoutes(WebServerService& server)
+{
+    server.on("/api/system", HTTP_GET,
+    [](AsyncWebServerRequest* request)
+    {
+        JsonDocument doc;
+
+        doc["firmware"] = VERSION;
+
+        doc["wifi"] = WiFi.RSSI();
+
+        doc["rooms"] = roomManager.count();
+
+        doc["channels"] = ioManager.count();
+
+        doc["scenes"] = sceneManager.count();
+
+        doc["schedules"] = 0;
+
+        doc["sensors"] = 0;
+
+        String json;
+
+        serializeJson(doc, json);
+
+        request->send(
+            200,
+            "application/json",
+            json);
+    });
+}
