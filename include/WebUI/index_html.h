066191fb1f3b35a0d94f56183bad1b200a2790a8
hatako77 <37298920+hatako77@users.noreply.github.com>
2026-07-13 22:58:11 +0330

    Update index_html.h

--- include/WebUI/index_html.h
+++ include/WebUI/index_html.h
@@ -11,8 +11,6 @@ const char INDEX_HTML[] PROGMEM = R"rawliteral(
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<title>HomeSweetHome</title>
-
 <link rel="stylesheet" href="/style.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 
@@ -20,26 +18,16 @@ const char INDEX_HTML[] PROGMEM = R"rawliteral(
 <script src="https://cdn.jsdelivr.net/npm/sortablejs@1.15.2/Sortable.min.js"></script>
 <body>
 
-<!-- TOP BAR -->
 <header class="topbar">
-  <div class="logo">
-    <i class="fa-solid fa-house"></i> HomeSweetHome
-  </div>
-
-  <div class="status online">
-    <i class="fa-solid fa-circle"></i> Online
-  </div>
+    ...
 </header>
 
-<main class="container">
-
-<div class="layout">
+<main class="app">
 
     <aside id="sidebar"></aside>
 
     <section id="content"></section>
 
-</div>
 </main>
 
 <script src="/app.js"></script>
