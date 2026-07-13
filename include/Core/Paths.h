4bf7419d1fb74c3851c8833404f7ba3ef8c82208
hatako77 <37298920+hatako77@users.noreply.github.com>
2026-07-13 18:20:27 +0330

    Update Paths.h

--- include/Core/Paths.h
+++ include/Core/Paths.h
@@ -4,7 +4,6 @@ namespace Paths
 {
     constexpr char Rooms[]  = "/rooms.json";
     constexpr char IO[]     = "/io.json";
-    constexpr char Groups[] = "/groups.json";
     constexpr char Scenes[] = "/scenes.json";
     constexpr char Settings[] = "/settings.json";
 }
