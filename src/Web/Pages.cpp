d35accc915ec3344c30c51c02e0d9206531882be
hatako77 <37298920+hatako77@users.noreply.github.com>
2026-07-14 23:54:07 +0330

    Update Pages.cpp

--- src/Web/Pages.cpp
+++ src/Web/Pages.cpp
@@ -1,64 +1,66 @@
 #include "Web/Pages.h"
 #include "Web/WebServerService.h"
+
 #include "WebUI/index_html.h"
-#include "WebUI/style_css.h"
-#include "WebUI/app_js.h"
 #include "WebUI/ota_html.h"
+
+#include "Web/Assets/Css/Css.h"
+#include "Web/Assets/Js/Js.h"
+
 #include <LittleFS.h>
 
 namespace Pages
 {
+	void registerRoutes(WebServerService& web)
+	{
+		Serial.println("Pages Register");
 
-    void registerRoutes(WebServerService& web)
-    {
-        Serial.println("Pages Register");
-        auto& server = web.server();
-    
-        if(!LittleFS.begin(true))
-        {
-            Serial.println("LittleFS Mount Failed");
-            return;
-        }
-    
-        server.on("/", HTTP_GET,
-        [](AsyncWebServerRequest *request)
-        {
-            request->send_P(
-                200,
-                "text/html",
-                INDEX_HTML
-            );
-        });
-        
-        server.on("/style.css", HTTP_GET,
-        [](AsyncWebServerRequest *request)
-        {
-            request->send_P(
-                200,
-                "text/css",
-                STYLE_CSS
-            );
-        });
-        
-        server.on("/app.js", HTTP_GET,
-        [](AsyncWebServerRequest *request)
-        {
-            request->send_P(
-                200,
-                "application/javascript",
-                APP_JS
-            );
-        });
-        
-        server.on("/ota", HTTP_GET,
-        [](AsyncWebServerRequest *request)
-        {
-            request->send_P(
-                200,
-                "text/html",
-                OTA_HTML
-            );
-        });
-    }
+		auto& server = web.server();
 
+		if(!LittleFS.begin(true))
+		{
+			Serial.println("LittleFS Mount Failed");
+			return;
+		}
+
+		server.on("/", HTTP_GET,
+		[](AsyncWebServerRequest* request)
+		{
+			request->send(
+				200,
+				"text/html",
+				INDEX_HTML
+			);
+		});
+
+		server.on("/style.css", HTTP_GET,
+		[](AsyncWebServerRequest* request)
+		{
+			request->send(
+				200,
+				"text/css",
+				CSS
+			);
+		});
+
+		server.on("/app.js", HTTP_GET,
+		[](AsyncWebServerRequest* request)
+		{
+			request->send(
+				200,
+				"application/javascript",
+				JS
+			);
+		});
+
+		server.on("/ota", HTTP_GET,
+		[](AsyncWebServerRequest* request)
+		{
+			request->send(
+				200,
+				"text/html",
+				OTA_HTML
+			);
+		});
+	}
 }
