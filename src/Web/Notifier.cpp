7b4a4571994df11f825f09e14a36d096a2eba94c
hatako77 <37298920+hatako77@users.noreply.github.com>
2026-07-15 18:34:56 +0330

    Update Notifier.cpp

--- src/Web/Notifier.cpp
+++ src/Web/Notifier.cpp
@@ -1,43 +1,46 @@
 #include "Web/Notifier.h"
-#include "Core/Scene.h"
 
+#include "Core/Scene.h"
+#include "Web/Message.h"
 
 extern WebSocketService websocket;
 
 void Notifier::reload()
 {
-    Message msg("system","reload");
+    Message msg("system", "reload");
+
     websocket.send(msg);
 }
+
 void Notifier::sceneAdded(const Scene& scene)
 {
-    Message msg("scene","added");
+    Message msg("scene", "added");
 
-    msg.data["id"] = scene.id;
-    msg.data["name"] = scene.name;
-    msg.data["icon"] = scene.icon;
+    msg.data["id"]       = scene.id;
+    msg.data["name"]     = scene.name;
+    msg.data["icon"]     = scene.icon;
     msg.data["favorite"] = scene.favorite;
-    msg.data["enabled"] = scene.enabled;
+    msg.data["enabled"]  = scene.enabled;
 
     websocket.send(msg);
 }
 
 void Notifier::sceneUpdated(const Scene& scene)
 {
-    Message msg("scene","updated");
+    Message msg("scene", "updated");
 
-    msg.data["id"] = scene.id;
-    msg.data["name"] = scene.name;
-    msg.data["icon"] = scene.icon;
+    msg.data["id"]       = scene.id;
+    msg.data["name"]     = scene.name;
+    msg.data["icon"]     = scene.icon;
     msg.data["favorite"] = scene.favorite;
-    msg.data["enabled"] = scene.enabled;
+    msg.data["enabled"]  = scene.enabled;
 
     websocket.send(msg);
 }
 
 void Notifier::sceneRemoved(uint16_t id)
 {
-    Message msg("scene","removed");
+    Message msg("scene", "removed");
 
     msg.data["id"] = id;
 
@@ -46,7 +49,7 @@ void Notifier::sceneRemoved(uint16_t id)
 
 void Notifier::sceneExecuted(uint16_t id)
 {
-    Message msg("scene","executed");
+    Message msg("scene", "executed");
 
     msg.data["id"] = id;
 
@@ -55,10 +58,17 @@ void Notifier::sceneExecuted(uint16_t id)
 
 void Notifier::channelChanged(const IOChannel& channel)
 {
-    Message msg("channel","changed");
+    Message msg("channel", "changed");
 
-    msg.data["id"] = channel.id;
-    msg.data["state"] = channel.state;
+    msg.data["id"]        = channel.id;
+    msg.data["roomId"]    = channel.roomId;
+    msg.data["name"]      = channel.name;
+    msg.data["state"]     = channel.state;
+    msg.data["enabled"]   = channel.enabled;
+    msg.data["favorite"]  = channel.favorite;
+    msg.data["activeLow"] = channel.activeLow;
+    msg.data["type"]      = (uint8_t)channel.type;
+    msg.data["icon"]      = (uint8_t)channel.icon;
 
     websocket.send(msg);
 }
@@ -68,24 +78,24 @@ void Notifier::otaStatus(
     const String& current,
     const String& remote)
 {
-    Message msg("ota","status");
+    Message msg("ota", "status");
 
-    msg.data["running"] = status.running;
-    msg.data["finished"] = status.finished;
-    msg.data["success"] = status.success;
+    msg.data["running"]    = status.running;
+    msg.data["finished"]   = status.finished;
+    msg.data["success"]    = status.success;
 
     msg.data["downloaded"] = status.downloaded;
-    msg.data["total"] = status.total;
+    msg.data["total"]      = status.total;
 
-    msg.data["percent"] = status.percent;
-    msg.data["speed"] = status.speedKB;
-    msg.data["eta"] = status.eta;
+    msg.data["percent"]    = status.percent;
+    msg.data["speed"]      = status.speedKB;
+    msg.data["eta"]        = status.eta;
 
-    msg.data["state"] = status.state;
-    msg.data["error"] = status.error;
+    msg.data["state"]      = status.state;
+    msg.data["error"]      = status.error;
 
-    msg.data["current"] = current;
-    msg.data["remote"] = remote;
+    msg.data["current"]    = current;
+    msg.data["remote"]     = remote;
 
     websocket.send(msg);
 }
