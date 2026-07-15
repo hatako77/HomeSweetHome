4a9fe213a0bec62601565c8eafc63f78b2912c73
hatako77 <37298920+hatako77@users.noreply.github.com>
2026-07-15 10:14:18 +0330

    Create Css.h

---
+++ include/Web/Assets/Css/Css.h
@@ -0,0 +1,42 @@
+#ifndef CSS_H
+#define CSS_H
+
+#include <pgmspace.h>
+
+#include "Web/Assets/Css/BaseCss.h"
+#include "Web/Assets/Css/LayoutCss.h"
+#include "Web/Assets/Css/SidebarCss.h"
+#include "Web/Assets/Css/CardCss.h"
+#include "Web/Assets/Css/ButtonCss.h"
+#include "Web/Assets/Css/FormCss.h"
+#include "Web/Assets/Css/DashboardCss.h"
+#include "Web/Assets/Css/RoomCss.h"
+#include "Web/Assets/Css/TileCss.h"
+#include "Web/Assets/Css/DialogCss.h"
+#include "Web/Assets/Css/ToastCss.h"
+#include "Web/Assets/Css/LoaderCss.h"
+#include "Web/Assets/Css/OTACss.h"
+#include "Web/Assets/Css/ResponsiveCss.h"
+
+const char CSS[] PROGMEM=R"rawliteral(
+
+)rawliteral"
+BASE_CSS
+LAYOUT_CSS
+SIDEBAR_CSS
+CARD_CSS
+BUTTON_CSS
+FORM_CSS
+DASHBOARD_CSS
+ROOM_CSS
+TILE_CSS
+DIALOG_CSS
+TOAST_CSS
+LOADER_CSS
+OTA_CSS
+RESPONSIVE_CSS
+R"rawliteral(
+
+)rawliteral";
+
+#endif
