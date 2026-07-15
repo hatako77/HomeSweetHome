948ccbfc537cfb51e7b43f29aa77fbf44348d78b
hatako77 <37298920+hatako77@users.noreply.github.com>
2026-07-15 23:18:20 +0330

    Update index_html.h

--- include/WebUI/index_html.h
+++ include/WebUI/index_html.h
@@ -31,8 +31,28 @@ const char INDEX_HTML[] PROGMEM = R"rawliteral(
 
 </div>
 
-<script src="/app.js"></script>
+<script src="/js/state.js"></script>
+<script src="/js/router.js"></script>
+<script src="/js/app.js"></script>
 
+<script src="/js/utils/dom.js"></script>
+<script src="/js/utils/api.js"></script>
+<script src="/js/utils/icons.js"></script>
+<script src="/js/utils/websocket.js"></script>
+
+<script src="/js/components/sidebar.js"></script>
+<script src="/js/components/dialog.js"></script>
+<script src="/js/components/modal.js"></script>
+<script src="/js/components/toast.js"></script>
+<script src="/js/components/loader.js"></script>
+
+<script src="/js/dashboard.js"></script>
+<script src="/js/room.js"></script>
+<script src="/js/scene.js"></script>
+<script src="/js/schedule.js"></script>
+<script src="/js/sensor.js"></script>
+<script src="/js/ota.js"></script>
+<script src="/js/settings.js"></script>
 </body>
 </html>
 
