fef8b93b0dbbdf19517896b8c77bb3407a9911e9
hatako77 <37298920+hatako77@users.noreply.github.com>
2026-07-15 11:04:54 +0330

    Update Pages.cpp

--- src/Web/Pages.cpp
+++ src/Web/Pages.cpp
@@ -19,6 +19,7 @@
 #include "Web/Assets/Css/OTACss.h"
 #include "Web/Assets/Css/ResponsiveCss.h"
 
+#include "Web/Assets/Js/AppJs.h"
 #include "Web/Assets/Js/DashboardJs.h"
 #include "Web/Assets/Js/RoomJs.h"
 #include "Web/Assets/Js/SceneJs.h"
