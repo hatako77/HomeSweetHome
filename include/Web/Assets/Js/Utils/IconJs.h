b9df4907e5cb349e1f5f1d1f9de1c231089e47af
hatako77 <37298920+hatako77@users.noreply.github.com>
2026-07-20 10:35:14 +0330

    Update IconJs.h

--- include/Web/Assets/Js/Utils/IconJs.h
+++ include/Web/Assets/Js/Utils/IconJs.h
@@ -9,19 +9,28 @@ function getIcon(id)
 {
     switch(Number(id))
     {
-        case 0: return Icons.light;
-        case 1: return Icons.fan;
-        case 2: return Icons.door;
-        case 3: return Icons.lock;
-        case 4: return Icons.motion;
-        case 5: return Icons.temperature;
-        case 6: return Icons.water;
-        case 7: return Icons.outlet;
-        case 8: return Icons.bell;
-        case 9: return Icons.curtain;
-        case 10: return Icons.tv;
-        default: return Icons.generic;
+    case 0:return Icons.light;
+    case 1:return Icons.fan;
+    case 2:return Icons.door;
+    case 3:return Icons.lock;
+    case 4:return Icons.motion;
+    case 5:return Icons.temperature;
+    case 6:return Icons.water;
+    case 7:return Icons.outlet;
+    case 8:return Icons.bell;
+    case 9:return Icons.curtain;
+    case 10:return Icons.tv;
+    default:return Icons.generic;
     }
+}
+
+function icon(id,size=22)
+{
+return `
+<span class="svg-icon" style="width:${size}px;height:${size}px">
+${getIcon(id)}
+</span>
+`;
 }
 
 )rawliteral";
