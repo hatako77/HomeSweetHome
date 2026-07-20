8a8184374e06843af87bffea1cdd3d8fc680b83b
hatako77 <37298920+hatako77@users.noreply.github.com>
2026-07-20 09:51:00 +0330

    Create SvgIcons.h

---
+++ include/Web/Assets/Js/Utils/SvgIcons.h
@@ -0,0 +1,26 @@
+#ifndef SVG_ICONS_H
+#define SVG_ICONS_H
+
+#include <pgmspace.h>
+
+const char SVG_ICONS[] PROGMEM = R"rawliteral(
+
+window.Icons =
+{
+    light:``,
+    fan:``,
+    door:``,
+    lock:``,
+    motion:``,
+    temperature:``,
+    water:``,
+    outlet:``,
+    bell:``,
+    curtain:``,
+    tv:``,
+    generic:``
+};
+
+)rawliteral";
+
+#endif
