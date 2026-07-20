3458a5419481946510c7bb62a8b524b012182fdd
hatako77 <37298920+hatako77@users.noreply.github.com>
2026-07-20 21:35:38 +0330

    Update Pages.cpp

--- src/Web/Pages.cpp
+++ src/Web/Pages.cpp
@@ -19,7 +19,6 @@
 #include "Web/Assets/Css/ResponsiveCss.h"
 
 #include "Web/Assets/Js/StateJs.h"
-#include "Web/Assets/Js/SortableJs.h"
 #include "Web/Assets/Js/ChannelJs.h"
 #include "Web/Assets/Js/RouterJs.h"
 #include "Web/Assets/Js/AppJs.h"
@@ -82,10 +81,6 @@ void registerRoutes(WebServerService& web)
         request->send(response);
     });
 
-    server.on("/js/libs/sortable.min.js",HTTP_GET,[](AsyncWebServerRequest* request)
-    {
-        request->send(200,"application/javascript",SORTABLE_JS);
-    });
 
     server.on("/js/utils/svgicons.js",HTTP_GET,[](AsyncWebServerRequest* request)
     {
