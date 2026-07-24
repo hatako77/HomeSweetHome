47e649148f13d7bd7bcbbe3a08802bfb75f5e5aa
hatako77 <37298920+hatako77@users.noreply.github.com>
2026-07-25 00:07:58 +0330

    Update RoomJs.h

--- include/Web/Assets/Js/RoomJs.h
+++ include/Web/Assets/Js/RoomJs.h
@@ -195,11 +195,11 @@ function createRoomCard(room)
                 }
 
                 <span>${room.name}</span>
-                <div> 
-                    <button style="background-color: #ffffff26;color: bisque;border-radius: 7rem;width: 2.5rem;height: 2.5rem;" onclick="editRoom(${room.id})">
+                <div style="display: flex;"> 
+                    <button style="align-items: center;justify-content: center;display: flex;background-color: #ffffff26;color: bisque;border-radius: 7rem;width: 2.5rem;height: 2.5rem;" onclick="editRoom(${room.id})">
                         ${icon("edit")}
                     </button>                
-                    <button style="background-color: #ffffff26;color: bisque;border-radius: 7rem;width: 2.5rem;height: 2.5rem;" onclick="deleteRoom(${room.id})">
+                    <button style="align-items: center;justify-content: center;display: flex;background-color: #ffffff26;color: bisque;border-radius: 7rem;width: 2.5rem;height: 2.5rem;" onclick="deleteRoom(${room.id})">
                         ${icon("delete")}
                     </button>
                 </div>
