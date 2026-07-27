6eb0205a58c559d08ca5fbd83dbc06cf99f2a1ca
hatako77 <37298920+hatako77@users.noreply.github.com>
2026-07-27 14:40:14 +0330

    Update SvgIcons.h

--- include/Web/Assets/Js/Utils/SvgIcons.h
+++ include/Web/Assets/Js/Utils/SvgIcons.h
@@ -16,22 +16,24 @@ stroke-linecap="round" stroke-linejoin="round">
 </svg>`,
 
 edit: `
-<svg viewBox="0 0 24 24" fill="none"
-stroke="currentColor" stroke-width="2"
-stroke-linecap="round" stroke-linejoin="round">
-    <path d="M4 20l4.5-1L19 8.5 15.5 5 5 15.5 4 20z"/>
-    <path d="M14.5 5.5L18 9"/>
+<svg xmlns="http://www.w3.org/2000/svg" width="800px" height="800px" viewBox="0 0 24 24" fill="none">
+<path d="M21.2799 6.40005L11.7399 15.94C10.7899 16.89 7.96987 17.33 7.33987 16.7C6.70987 16.07 7.13987 13.25 8.08987 12.3L17.6399 2.75002C17.8754 2.49308 18.1605 2.28654 18.4781 2.14284C18.7956 1.99914 19.139 1.92124 19.4875 1.9139C19.8359 1.90657 20.1823 1.96991 20.5056 2.10012C20.8289 2.23033 21.1225 2.42473 21.3686 2.67153C21.6147 2.91833 21.8083 3.21243 21.9376 3.53609C22.0669 3.85976 22.1294 4.20626 22.1211 4.55471C22.1128 4.90316 22.0339 5.24635 21.8894 5.5635C21.7448 5.88065 21.5375 6.16524 21.2799 6.40005V6.40005Z" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" style="
+    stroke: rgb(255 255 255);
+"></path>
+<path d="M11 4H6C4.93913 4 3.92178 4.42142 3.17163 5.17157C2.42149 5.92172 2 6.93913 2 8V18C2 19.0609 2.42149 20.0783 3.17163 20.8284C3.92178 21.5786 4.93913 22 6 22H17C19.21 22 20 20.2 20 18V13" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" style="
+    stroke: rgb(255 255 255);
+"></path>
 </svg>`,
 
 delete: `
-<svg viewBox="0 0 24 24" fill="none"
-stroke="currentColor" stroke-width="2"
-stroke-linecap="round" stroke-linejoin="round">
-    <path d="M4 7h16"/>
-    <path d="M9 4h6"/>
-    <path d="M7 7l1 13h8l1-13"/>
-    <path d="M10 11v6"/>
-    <path d="M14 11v6"/>
+<svg xmlns="http://www.w3.org/2000/svg" fill="#000000" width="800px" height="800px" viewBox="0 0 1920 1920" style="
+    /* color: white; */
+">
+    <path d="M1581.176 1750.588c0 31.06-25.411 56.47-56.47 56.47H395.294c-31.059 0-56.47-25.41-56.47-56.47V564.706H225.882v1185.882c0 93.403 76.01 169.412 169.412 169.412h1129.412c93.402 0 169.412-76.01 169.412-169.412V564.706h-112.942v1185.882Zm-903.529-169.412h112.941V677.647h-112.94v903.53Zm451.765 0h112.94V677.647h-112.94v903.53Zm211.211-1242.352L1217.065 0H694.6L571.268 338.824H.01v112.94h1920v-112.94h-579.388Zm-649.299 0 82.334-225.883h364.462l82.334 225.883h-529.13Z" fill-rule="evenodd" style="
+    stroke: rgb(255 255 255);
+    fill-rule: evenodd;
+    fill: white;
+"></path>
 </svg>`,
 
 create: `
