2860ace9c36cdd3adc7c36beca272b9ff6083fb5
hatako77 <37298920+hatako77@users.noreply.github.com>
2026-07-31 14:07:17 +0330

    Update SceneAction.h

--- include/Core/SceneAction.h
+++ include/Core/SceneAction.h
@@ -6,7 +6,13 @@
 struct SceneAction
 {
     uint16_t channelId = 0;
+
     bool state = false;
+
+    // Delay before execution
+    uint32_t delayMs = 0;
+
+    // How long the state should remain active
     uint32_t durationMs = 0;
 };
 
