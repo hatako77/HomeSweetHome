fa4d5aa8e004b3e165b3af1f05fbcd033baaec36
hatako77 <37298920+hatako77@users.noreply.github.com>
2026-08-03 19:30:52 +0330

    Update SceneJs.h

--- include/Web/Assets/Js/SceneJs.h
+++ include/Web/Assets/Js/SceneJs.h
@@ -34,7 +34,6 @@ function showScenes()
 function renderScenes()
 {
     if(App.currentPage !== "scenes") return;
-    if(!container) return;
     const list = $("sceneList");
     if(!list) return;
     list.innerHTML = "";
